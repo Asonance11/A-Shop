2bc525f94b2fc5e592b185753d03ccd2427b8313
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-24 12:46:20 +0100

    Styling of Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
 	return (
-		<header>
-			<h1>
+		<header className="flex justify-between items-center w-full py-4 px-8 bg-darkGrey fixed">
+			<h1 className="font-serif italic font-semibold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-forestGreen via-adventurousYellow to-foggyBlue">
 				<Link to="/">Painty</Link>
 			</h1>
 
-			<ul>
-				<li>
+			<ul className="flex items-center gap-6 text-white font-sans">
+				<li className="text-xl font-medium hover:underline">
 					<Link to="/shop">Shop </Link>
 				</li>
-				<li>
+				<li className="text-xl font-medium hover:underline">
 					<Link to="/cart">Cart </Link>
 				</li>
 			</ul>
