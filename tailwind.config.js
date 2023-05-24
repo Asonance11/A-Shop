4b0d997e36523e7401059f44626dfd9398816394
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-24 12:32:11 +0100

    refactor: Change of additional colors to extend object

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,19 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		colors: {
-			forestGreen: '#a9f6ad',
-			foggyBlue: '#a9dbf6',
-			oceanTurquoise: '#a9f6e8',
-			adventurousYellow: '#ecf6a9',
-			darkGrey: '#232323',
-		},
 		extend: {
 			fontFamily: {
 				sans: ['Lato', 'sans-serif'],
 				serif: ['Cormorant Garamond', 'serif'],
+			},
+			colors: {
+				transparent: 'transparent',
+				current: 'currentColor',
+				forestGreen: '#a9f6ad',
+				foggyBlue: '#a9dbf6',
+				oceanTurquoise: '#a9f6e8',
+				adventurousYellow: '#ecf6a9',
+				darkGrey: '#232323',
 			},
 		},
 	},
