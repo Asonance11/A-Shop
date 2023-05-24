49ec4230b205c9d203cb61513a00784adcbbc704
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-24 11:30:10 +0100

    Setup of routes to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import Shop from './pages/Shop';
 
 const App = () => {
 	return (
 		<div className="">
 			<Nav />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/shop" element={<Shop />} />
+					<Route path="/cart" element={<Cart />} />
+					<Route path="/shop/:id" element={<Product />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 };
