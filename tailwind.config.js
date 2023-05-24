0e9e8526d8173180763b9d47c41fb463cb5a532c
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-24 12:05:39 +0100

    Addition of custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		colors: {
+			forestGreen: '#a9f6ad',
+			foggyBlue: '#a9dbf6',
+			oceanTurquoise: '#a9f6e8',
+			adventurousYellow: '#ecf6a9',
+			darkGrey: '#232323',
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Lato', 'sans-serif'],
+				serif: ['Cormorant Garamond', 'serif'],
+			},
+		},
 	},
 	plugins: [],
 };
