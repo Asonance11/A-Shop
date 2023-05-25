cbe1750ff8bca5e1449e709d63a2c4b005444c34
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-25 15:46:00 +0100

    SHop conponent

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Item from '../components/Item';
 
-const Shop = () => {
-	return <div>Shop</div>;
+const Shop = ({ products }) => {
+	return (
+		<main>
+			<h2>Products</h2>
+
+			<div>
+				{products.map((item) => {
+					<Item product={item} key={item.num} />;
+				})}
+			</div>
+		</main>
+	);
 };
 
 export default Shop;
