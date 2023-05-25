08e8191a5ecac748e548bf376d726480b83163a6
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-25 13:50:02 +0100

    update of parameters from 15 to 20 images

--- src/API/getProducts.js
+++ src/API/getProducts.js
@@ -1,7 +1,7 @@
 import API from '../../config';
 
 export default async function fetchProducts() {
-	let response = await fetch('https://api.pexels.com/v1/curated', {
+	let response = await fetch('https://api.pexels.com/v1/curated/?per_page=20', {
 		headers: {
 			Authorization: `${API.key}`,
 		},
