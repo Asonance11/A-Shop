01b7b0dce89069f007952395d15d27a8dbb03a25
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-26 13:13:38 +0100

    creation of product component

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,13 +8,41 @@ const Product = () => {
 	useEffect(() => {
 		fetchOneProduct(id).then((data) => {
 			setProduct(data);
-			console.log(data);
 		});
 	}, []);
 
 	return (
-		<section className="pt-24">
-			<h2>Product</h2>
+		<section className="pt-24 bg-darkGrey px-8 min-h-screen">
+			<h2 className="text-3xl text-white font-bold font-serif italic text-center">
+				Product
+			</h2>
+			<div className="grid grid-cols-2 gap-8 mt-8 font-sans items-center">
+				<img src={product.image} alt={product.alt} className="w-full" />
+				<ul className="text-white flex flex-col gap-4 ">
+					<li className="flex items-center justify-between">
+						<p>Photographer:</p>
+						<a href={product.photographerUrl} className="hover:underline">
+							{product.photographer}
+						</a>
+					</li>
+					<li className="flex items-center justify-between">
+						<p>Price:</p>
+						<p>{`$${product.price}`}</p>
+					</li>
+					<li className="flex items-center justify-between">
+						<p>Image size</p>
+						<p>{product.imageSize}</p>
+					</li>
+					<li className="flex items-center justify-between">
+						<button
+							type="button"
+							className="py-2 px-4 bg-gradient-to-r from-forestGreen via-adventurousYellow to-oceanTurquoise rounded-md text-darkGrey transition-all duration-500 ease-in-out   hover:from-foggyBlue hover:to-forestGreen"
+						>
+							Add to Cart
+						</button>
+					</li>
+				</ul>
+			</div>
 		</section>
 	);
 };
