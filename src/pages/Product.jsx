06246528e5e61fd1859413cce4f03bb8e877169c
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-26 12:46:27 +0100

    useEffect hook to get the particular product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchOneProduct } from '../API/getProducts';
 
 const Product = () => {
-	return <div>Product</div>;
+	const [product, setProduct] = useState({});
+	const { id } = useParams();
+	useEffect(() => {
+		fetchOneProduct(id).then((data) => {
+			setProduct(data);
+			console.log(data);
+		});
+	}, []);
+
+	return (
+		<section className="pt-24">
+			<h2>Product</h2>
+		</section>
+	);
 };
 
 export default Product;
