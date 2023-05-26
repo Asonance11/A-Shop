97d489428c647105ea78d0e41077f4c4b1044eb5
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-26 12:20:16 +0100

    function to a particular picture from the API

--- src/API/getProducts.js
+++ src/API/getProducts.js
@@ -24,3 +24,27 @@ export default async function fetchProducts() {
 	});
 	return products;
 }
+
+export async function fetchOneProduct(id) {
+	let response = await fetch(`https://api.pexels.com/v1/photos/${id}`, {
+		headers: {
+			Authorization: `${API.key}`,
+		},
+	});
+
+	response = await response.json();
+
+	const product = {
+		id: response.id,
+		photographer: response.photographer,
+		photographerUrl: response.photographer_url,
+		image: response.src.landscape,
+		url: response.url,
+		imageSize: `${response.width} x ${response.height}`,
+		alt: response.alt,
+		price: response.height,
+		avgColor: response.avg_color,
+	};
+
+	return product;
+}
