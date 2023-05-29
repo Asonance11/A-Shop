c63a64a7ef0efef68ed43b9df1a039ee6b279757
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-29 13:56:03 +0100

    test: test click event  add to cart button and increment button

--- src/tests/ProductItem.test.jsx
+++ src/tests/ProductItem.test.jsx
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { describe, it } from 'vitest';
+import { describe, it, vi } from 'vitest';
 import ProductItem from '../components/ProductItem';
 
 describe('Product component', () => {
@@ -18,5 +19,49 @@ describe('Product component', () => {
 			wrapper: MemoryRouter,
 		});
 		expect(container).toMatchSnapshot();
+	});
+
+	it('calls setCart when add to cart button is clicked', async () => {
+		const setCart = vi.fn();
+		const anItem = {
+			image: 'www.awebsite.com/something.jpeg',
+			alt: 'An image',
+			photographer: 'me',
+			price: 10000,
+			imageSize: '3000 x 3000',
+		};
+		const user = userEvent.setup();
+
+		render(<ProductItem product={anItem} cart={[]} setCart={setCart} />);
+
+		const addToCartButton = screen.getByRole('button', { name: 'Add to Cart' });
+
+		await user.click(addToCartButton);
+		expect(setCart).toHaveBeenCalled();
+		expect(setCart).toHaveBeenCalledWith([{ quantity: 1, ...anItem }]);
+	});
+
+	it('calls setCart with the correct quantity is clicked', async () => {
+		const setCart = vi.fn();
+		const anItem = {
+			image: 'www.awebsite.com/something.jpeg',
+			alt: 'An image',
+			photographer: 'me',
+			price: 10000,
+			imageSize: '3000 x 3000',
+		};
+		const user = userEvent.setup();
+
+		render(<ProductItem product={anItem} cart={[]} setCart={setCart} />);
+
+		const addToCartButton = screen.getByRole('button', { name: 'Add to Cart' });
+		const incrementButton = screen.getByRole('button', { name: '+' });
+
+		await user.click(incrementButton);
+		await user.click(incrementButton);
+		await user.click(incrementButton);
+		await user.click(addToCartButton);
+		expect(setCart).toHaveBeenCalled();
+		expect(setCart).toHaveBeenCalledWith([{ quantity: 4, ...anItem }]);
 	});
 });
