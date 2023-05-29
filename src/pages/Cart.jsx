42ce5b8820f38ccdb2586155512e2791c3e0f1a4
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-29 15:04:19 +0100

    Creation of cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import CartItem from '../components/CartItem';
 
-const Cart = () => {
-	return <div>Cart</div>;
+const Cart = ({ cart, setCart }) => {
+	const items = cart.map((item, index) => (
+		<CartItem item={item} cart={cart} setCart={setCart} key={index + 100} />
+	));
+
+	return (
+		<main className="pt-24 bg-darkGrey px-8 min-h-screen">
+			<h2 className="text-3xl text-white font-bold font-serif italic text-center">
+				Your Cart
+			</h2>
+
+			<section>{items}</section>
+		</main>
+	);
 };
 
 export default Cart;
