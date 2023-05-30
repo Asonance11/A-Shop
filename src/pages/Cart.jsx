12b25b171d06edd87d30ffa5304764b79cb63c35
Oluwadabira <adigunsamuelaso@gmail.com>
2023-05-30 09:11:41 +0100

    function to calculate the total pricce of items in the cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import CartItem from '../components/CartItem';
 
 const Cart = ({ cart, setCart }) => {
+	const [totalPrice, setTotalPrice] = useState(0);
+
+	useEffect(() => {
+		setTotalPrice(cart.reduce(getTotal, 0));
+		function getTotal(total, item) {
+			return total + item.price * item.quantity;
+		}
+	}, [cart]);
+
 	const items = cart.map((item, index) => (
 		<CartItem item={item} cart={cart} setCart={setCart} key={index + 100} />
 	));
@@ -13,6 +22,11 @@ const Cart = ({ cart, setCart }) => {
 			</h2>
 
 			<section className="flex flex-col gap-4 mt-4">{items}</section>
+
+			<section className="flex items-center justify-between mt-2 text-white">
+				<p>Total Price:</p>
+				<h3>${totalPrice}</h3>
+			</section>
 		</main>
 	);
 };
